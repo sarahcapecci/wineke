95f016881b6f553fa34db9180bdf9d459be93667
Sarah Capecci <shlcapecci@gmail.com>
2014-08-07 18:44:10 -0400

    Page templates working with Angular

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,11 +3,10 @@ var winekeWeb = angular.module('winekeWeb', ['ngRoute']);
 winekeWeb.config(['$routeProvider', function($routeProvider){
     var directory = "../../";
     $routeProvider
-
     // route for the home page
     .when('/home', {
-        templateUrl : directory + 'wineke.html',
-        controller  : 'mainController'
+        templateUrl : directory + 'home.html',
+        controller  : 'MainController'
     })
 
     // route for the about page
@@ -28,13 +27,15 @@ winekeWeb.config(['$routeProvider', function($routeProvider){
 
     // route for the services page
     .when('/services', {
-        templateUrl : directory + 'pages/services.html'
+        templateUrl : directory + 'pages/services.html',
+        title: 'Services'
+
     })
 
     // route for the contact page
     .when('/contact', {
         templateUrl : directory + 'pages/contact.html',
-        controller  : 'contactController'
+        controller : 'ContactController'
     });
 
     // otherwise({
@@ -44,11 +45,39 @@ winekeWeb.config(['$routeProvider', function($routeProvider){
     // $locationProvider.html5Mode(true);
 }]);
 
-winekeWeb.controller('mainController', function($scope){
 
-});
+winekeWeb.controller('MainController', ['$scope', function($scope){
+   
 
-winekeWeb.controller('contactController', function($scope){
+}]);
 
-});
+winekeWeb.controller('ContactController', ['$route', function($scope, $http){
+    $scope.formData = {};
+      // submission message doesn't show when page loads
+      $scope.submission = false;
+      $scope.submitForm = function() {
+        $http({
+        method : 'POST',
+        url : 'process.php',
+        data : $.param($scope.formData), // pass in data as strings
+        headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+      })
+        .success(function(data) {
+          if (!data.success) {
+           // if not successful, bind errors to error variables
+           $scope.errorName = data.errors.name;
+           $scope.errorEmail = data.errors.email;
+           $scope.errorTextarea = data.errors.message;
+           $scope.submissionMessage = data.messageError;
+           $scope.submission = true; //shows the error message
+          } else {
+          // if successful, bind success message to message
+           $scope.submissionMessage = data.messageSuccess;
+           $scope.formData = {}; // form fields are emptied with this line
+           $scope.submission = true; //shows the sucess message
+          }
+         });
+       };
+}]);
+
 
